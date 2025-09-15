632dafd62848f68b5af1cb3332cf7bbbacf2e53d
Lohran Mendes <lohranfm@gmail.com>
2025-09-15 12:27:46 -0300

    Enhance ProductController with improved route handling and add Jackson dependency for JSON processing

--- src/main/java/edu/infnet/ProductController.java
+++ src/main/java/edu/infnet/ProductController.java
@@ -3,8 +3,13 @@ package edu.infnet;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProductController {
     private static final String resource = "products";
+
+    private static Map<String, Product> productsList = new HashMap<>();
 
     public static void config(Javalin app) {
 
@@ -15,48 +20,72 @@ public class ProductController {
         app.get(resource, ProductController::getProducts);
 
         // GET /products/id
-        app.get(resource + "/id", ProductController::getProductById);
+        app.get(resource + "/{id}", ProductController::getProductById);
 
         // POST /products
         app.post(resource, ProductController::insertProduct);
 
         // PUT /products/{id}
-        app.put(resource, ProductController::updateProduct);
+        app.put(resource + "/{id}", ProductController::updateProduct);
 
         // DELETE /products/{id
-        app.delete(resource, ProductController::deleteProduct);
+        app.delete(resource + "/{id}", ProductController::deleteProduct);
 
         // PATCH /products/{id
-        app.patch(resource + "/id", ProductController::patchProduct);
+        app.patch(resource + "/{id}", ProductController::patchProduct);
     }
 
     public static void getConnection(Context ctx) {
-        ctx.status(200).result("Conexão ok!");
+        ctx
+                .status(200)
+                .result("Conexão ok!");
     }
 
     public static void getProducts(Context ctx) {
-        ctx.status(200).result("Lista de produtos consultada.");
+        ctx
+                .status(200)
+                .json(productsList);
     }
 
     public static void getProductById(Context ctx) {
-        ctx.status(200).result("produto consultado por ID.");
+        String id = ctx
+                .pathParam("id");
+
+        ctx
+                .status(200)
+                .result("produto consultado pelo ID: " + id);
     }
 
     public static void insertProduct(Context ctx) {
-        ctx.status(200).result("Produto inserido.");
+        ctx
+                .status(200)
+                .result("Produto inserido.");
     }
 
     public static void updateProduct(Context ctx) {
-        ctx.status(200).result("produto atualizado.");
+        String id = ctx
+                .pathParam("id");
+
+        ctx
+                .status(200)
+                .result("produto atualizado.");
     }
 
     public static void deleteProduct(Context ctx) {
-        ctx.status(200).result("Produto deletado.");
+        String id = ctx
+                .pathParam("id");
+
+        ctx
+                .status(200)
+                .result("Produto deletado.");
     }
 
     public static void patchProduct(Context ctx) {
-        ctx.status(200).result("Produto atualizado parcialmente.");
+        String id = ctx
+                .pathParam("id");
+
+        ctx
+                .status(200)
+                .result("Produto atualizado parcialmente.");
     }
-
-
 }
