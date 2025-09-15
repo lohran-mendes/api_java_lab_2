842a6eaa0f3d2d703f343d5998f91be6d2e4b461
Lohran Mendes <lohranfm@gmail.com>
2025-09-15 11:50:51 -0300

    Refactor ProductController to centralize route configuration and enhance product management endpoints

--- src/main/java/edu/infnet/Main.java
+++ src/main/java/edu/infnet/Main.java
@@ -4,16 +4,9 @@ import io.javalin.Javalin;
 
 public class Main {
     public static void main(String[] args) {
+
         // Iniciar o servidor Javalin
         var app = Javalin.create().start(8080);
-
-        // Instanciar o controlador
-        ProductController productController = new ProductController();
-
-        // GET /
-        app.get("/", productController::getConnection);
-
-        // GET /products
-        app.get("/products", productController::getProductsList);
+        ProductController.config(app);
     }
 }
--- src/main/java/edu/infnet/ProductController.java
+++ src/main/java/edu/infnet/ProductController.java
@@ -1,17 +1,62 @@
 package edu.infnet;
 
+import io.javalin.Javalin;
 import io.javalin.http.Context;
 
 public class ProductController {
-    public void tratarRequisicao(Context ctx, Product product) {
+    private static final String resource = "products";
 
+    public static void config(Javalin app) {
+
+        // GET /
+        app.get("/", ProductController::getConnection);
+
+        // GET /products/
+        app.get(resource, ProductController::getProducts);
+
+        // GET /products/id
+        app.get(resource + "/id", ProductController::getProductById);
+
+        // POST /products
+        app.post(resource, ProductController::insertProduct);
+
+        // PUT /products/{id}
+        app.put(resource, ProductController::updateProduct);
+
+        // DELETE /products/{id
+        app.delete(resource, ProductController::deleteProduct);
+
+        // PATCH /products/{id
+        app.patch(resource + "/id", ProductController::patchProduct);
     }
-    public void getConnection(Context ctx) {
+
+    public static void getConnection(Context ctx) {
         ctx.status(200).result("Conexão ok!");
     }
 
-    public void getProductsList(Context ctx) {
-    ctx.status(200).result("Lista de produtos consultada.");
+    public static void getProducts(Context ctx) {
+        ctx.status(200).result("Lista de produtos consultada.");
     }
+
+    public static void getProductById(Context ctx) {
+        ctx.status(200).result("produto consultado por ID.");
+    }
+
+    public static void insertProduct(Context ctx) {
+        ctx.status(200).result("Produto inserido.");
+    }
+
+    public static void updateProduct(Context ctx) {
+        ctx.status(200).result("produto atualizado.");
+    }
+
+    public static void deleteProduct(Context ctx) {
+        ctx.status(200).result("Produto deletado.");
+    }
+
+    public static void patchProduct(Context ctx) {
+        ctx.status(200).result("Produto atualizado parcialmente.");
+    }
+
 
 }
